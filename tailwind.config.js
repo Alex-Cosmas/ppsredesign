b48c89f2c622e159eb5e7b7a658fbf70d14ed423
AlexCsmas <2alexcosmas@gmail.com>
2019-11-22 15:34:31 +0300

    First Website Commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    fontFamily:  {
+      body: ['Open Sans', 'sans-serif'],
+      display: ['Open Sans', 'sans-serif']
+    },
+
+    extend: {
+      colors: {
+        primary: '#192751',
+        secondary: '#989898', 
+        optional: '#73AE45',
+        gradient: '#cedac5',
+        
+        
+      },
+
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+      
+    }
+  },
+  variants: {}
+}
